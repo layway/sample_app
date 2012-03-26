f0d5eb8c3ba8f64dc7052da43624192a1f1b13a0
layway <layway1@gmail.com>
2012-03-26 21:05:07 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a42b243396f974d6ef6b768950c8a5ba1bda51fd4f4aa2a40922dd426de9a582d47c013c7ad6be7f4d01e5832a7d65bec2ad3faacd4675b91e2451ef463029cd'
